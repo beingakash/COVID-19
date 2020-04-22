7456fae636f9a1ee9ba6260783b63af3354dff02
beingAkash <beingakki26@gmail.com>
2020-04-23 01:44:57 +0530

    Build Commit 3

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+
+const port=process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+// app.get('/', function (req, res) {
+//  return res.send('pong');
+// });
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+
+
+
+app.listen(port, () => {
+
+  console.log('Server is up on the port '+port+'.')
+})
