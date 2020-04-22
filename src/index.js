be4218c1ccdb8639a370b1f5a098560ed6e037a6
beingAkash <beingakki26@gmail.com>
2020-04-23 01:14:31 +0530

    Build Commit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ import ReactDOM from 'react-dom'
 import App from './App';
 
 ReactDOM.render(<App />,document.getElementById('root'))
+
+// const express = require('express');
+// const app=express()
+// const path = require('path');
+// const port= process.env.PORT || 9000
+// const publicDirectoryPath = path.join(__dirname, 'App')
+// app.use(express.static(publicDirectoryPath))
+
+
+
+// app.listen(port, () => {
+
+//     console.log('Server is on PORT : '+port)
+// })
+
+
